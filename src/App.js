3db92973858cd44f92555cf0cc0bed1b63d5e2a1
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-04 03:00:32 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+
+  //Adding the local state for search input using useState hooks
+
+  const [searchTerm, setSearchTerm]= useState('')
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <form>
+        <label>
+          <span>Search for a book</span>
+          
+          <input
+            type="search"
+            placeholder="microservice, restful design, etc,"
+          />
+          <button type='submit'>Search</button>
+        </label>
+      </form>
+    </section>
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
