688a2ca4c364c1c04823feb6186555c564c9947b
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-04 03:28:02 +0300

    Search result to state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 
 const App = () => {
@@ -8,18 +9,40 @@ const App = () => {
   //Adding the local state for search input using useState hooks
 
   const [searchTerm, setSearchTerm]= useState('')
+  const [books, setBooks] = useState({ items: [] })
   const onInputChange = (e) => {
     setSearchTerm(e.target.value)
   }
+
+  let API_URL = `https://www.googleapis.com/books/v1/volumes`
+
+  const fetchBooks = async () => {
+    //The call to API
+    const result = await axios.get(`${API_URL}?q=${searchTerm}`)
+    //
+   // console.log(result.data)
+   setBooks(result.data)
+  }
+
+  //The submit handler
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    //fetchbooks calls the google books API
+    fetchBooks()
+  }
   return (
     <section>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <label>
           <span>Search for a book</span>
           
           <input
             type="search"
             placeholder="microservice, restful design, etc,"
+            //
+            value={searchTerm}
+            onChange={onInputChange}
           />
           <button type='submit'>Search</button>
         </label>
