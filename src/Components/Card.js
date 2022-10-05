6ac3f12506d7bd56acc1e6cad5395de8c2bc5201
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-05 20:14:28 +0300

    Redirect to google books

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 const Card = ({ book }) => {
-  console.log(book);
+  //console.log(book);
+  const [show, setShow] = useState(false)
+  const [bookItem, setBookItem]=useState()
+
+
   return (
     <React.Fragment>
       {book.map((item) => {
         let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
-        let amount = item.saleInfo.saleability
+        //let amount = item.saleInfo.saleability
         if (thumbnail!= undefined)
         {
             return (
                 <React.Fragment>
-                  <div className="card">
+                  <div className="card" onClick={()=>{setShow(true); setBookItem(item)}}>
                     <img src={thumbnail} alt="" />
                     <div className="bottom">
                       <h3 className="title">{item.volumeInfo.title}</h3>
                       {/* <p className="amount">&#36;25</p> */}
                     </div>
                   </div>
+                    <Modal show={show} item={bookItem} onClose={() => setShow(false)}/>
                   </React.Fragment>
                 );
         }
