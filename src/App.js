fc74684002e8c2dc8b5ddbe04bd788c874cff059
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-05 14:50:34 +0300

    Styling and display

--- src/App.js
+++ src/App.js
@@ -1,96 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import Main from './Components/Main'
+import './Components/style.css'
 
-
-const App = () => {
-
-  //Adding the local state for search input using useState hooks
-
-  const [searchTerm, setSearchTerm]= useState('')
-  const [books, setBooks] = useState({ items: [] })
-  const onInputChange = (e) => {
-    setSearchTerm(e.target.value)
-  }
-
-  let API_URL = `https://www.googleapis.com/books/v1/volumes`
-
-  const fetchBooks = async () => {
-    //The call to API
-    const result = await axios.get(`${API_URL}?q=${searchTerm}`)
-    //
-   // console.log(result.data)
-   setBooks(result.data)
-  }
-
-  //The submit handler
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault()
-    //fetchbooks calls the google books API
-    fetchBooks()
-  }
-  return (
-    <section>
-      <form onSubmit={onSubmitHandler}>
-        <label>
-          <span>Search for a book</span>
-          
-          <input
-            type="search"
-            placeholder="microservice, restful design, etc,"
-            //
-            value={searchTerm}
-            onChange={onInputChange}
-          />
-          <button type='submit'>Search</button>
-        </label>
-      </form>
-
-      {/* UI for books search result */}
-      <ul>
-        {
-          books.items.map((book, index) => {
-            return (
-              <li key={index}>
-                <div>
-                  <img alt = {`${book.volumeInfo.title}book`} src={`http://books.goog;e.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
-                  <div>
-                    <h3>{book.volumeInfo.title}</h3>
-                    <p>{book.volumeInfo.publishedDate}</p>
-                  </div>
-                
-                </div>
-                <hr />
-
-              </li>
-            )
-          })}
-      </ul>
-    </section>
-  )
+function App() {
+    return (
+        <React.Fragment>
+            <Main/>
+        </React.Fragment>
+    )
 }
 
-// function App() {
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import { useState } from 'react';
+// import axios from 'axios';
+
+
+// const App = () => {
+
+//   //Adding the local state for search input using useState hooks
+
+//   const [searchTerm, setSearchTerm]= useState('')
+//   const [books, setBooks] = useState({ items: [] })
+//   const onInputChange = (e) => {
+//     setSearchTerm(e.target.value)
+//   }
+
+//   let API_URL = `https://www.googleapis.com/books/v1/volumes`
+
+//   const fetchBooks = async () => {
+//     //The call to API
+//     const result = await axios.get(`${API_URL}?q=${searchTerm}`)
+//     //
+//    console.log(result.data)
+//    setBooks(result.data)
+//   }
+
+//   //The submit handler
+
+//   const onSubmitHandler = (e) => {
+//     e.preventDefault()
+//     //fetchbooks calls the google books API
+//     fetchBooks()
+//   }
+
+//   //Looking at the authors next
+
+//   const bookAuthors = authors => {
+//     if (authors.length <= 2) {
+//       authors = authors.join( ' and ')
+//     } else if (authors.length > 2) {
+//       let lastAuthor = ' and ' + authors.slice(-1)
+//       authors.pop()
+//       authors = authors.join(', ')
+//       authors += lastAuthor
+//     }
+//     return authors
+//   }
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
+//     <section>
+//       <form onSubmit={onSubmitHandler}>
+//         <label>
+//           <span>Search for a book</span>
+          
+//           <input
+//             type="search"
+//             placeholder="microservice, restful design, etc,"
+//             //
+//             value={searchTerm}
+//             onChange={onInputChange}
+//           />
+//           <button type='submit'>Search</button>
+//         </label>
+//       </form>
+
+//       {/* UI for books search result */}
+//       <ul>
+//         {
+//           books.items.map((book, index) => {
+//             return (
+//               <li key={index}>
+//                 <div>
+//                   <img alt = {`${book.volumeInfo.title}book`} src={`http://books.goog;e.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
+//                   <div>
+//                     <h3>{book.volumeInfo.title}</h3>
+//                     <p>{ bookAuthors(book.volumeInfo.authors)}</p>
+//                     <p>{book.volumeInfo.publishedDate}</p>
+//                   </div>
+                
+//                 </div>
+//                 <hr />
+
+//               </li>
+//             )
+//           })}
+//       </ul>
+//     </section>
+//   )
 // }
 
-export default App;
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <React.Fragment>
+
+            <div className="header">
+                <div className="row1">
+                    <h1>The more that you read the more you will know<br/>The more you learn the more places you will go</h1>
+                </div>
+                <div className="row2">
+                    <h2>Find A book</h2>
+                
+                    <div className="search">
+                        <input type="text" placeholder="Enter A Book Name"/>
+                        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+
+                    
+                    <img src="./images/img4.png" alt=""/>
+                </div>
+            </div>
+            </div>
+        </React.Fragment>
+    )
+}
+export default Main;
